212bdfdfa1a5d835ddd0d3b58c4547a3d850cef1
Gregory Delgado <gregd92694@yahoo.com>
2021-12-06 16:17:18 -0600

    App component started

---
+++ src/components/App/app.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+
+class App extends Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+        object: null,
+    }
+render() {
+    return (
+        <div className="container-fluid">
+<h1>Sports and stuff fan site</h1>
+        </div>
+    )
+}
+}
+export default App;
