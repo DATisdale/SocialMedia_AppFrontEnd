cccc5840e4611b5da6479a9acac6300d1089c4f3
Gregory Delgado <gregd92694@yahoo.com>
2021-12-07 16:24:23 -0600

    Folder created for images and appropriate pathing for first page was created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import {Switch, Route} from 'react-router-dom';
 import SearchBar from "./components/SearchBar/SearchBar";
-import PageLayout from "./components/PageLayout/PageLayout";
+import Home from "./components/Home/Home"
+// import PageLayout from "./components/PageLayout/PageLayout";
 
 class App extends Component {
     state = {
@@ -20,7 +21,9 @@ render() {
         <SearchBar/>
         <Switch>
             <Route path="/" exact component={Home}/>
-            <Route path="/login" exact component={Login}/>
+            {/* <Route path="/login" exact component={Login}/>
+            <Route path="/signup" exact component={Signup}/>
+            <Route path="/newsFeed" exact Component={NewsFeed}/>
             <Route path="/profile" render={(props) => {
                 if(!this.state.user){
                     return <Redirect to='/login' />
@@ -28,8 +31,8 @@ render() {
                     return <Profile {...props} />
                 }
             }}/>
-            <Route path="/friends" component={Friends}/>
-            <Redirect to='/not-found'/>
+            <Route path="/friends" component={Friends}/> */}
+            {/* <Redirect to='/not-found'/> */}
         </Switch>
         </div>
     )
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css";
+
+const Home=()=>{
+        return (
+            <section class ="home">
+            <div class="card" >
+                <div class="card-body">
+                  <p class="card-text">Login</p>
+                </div>
+              </div>
+        </section>
+          );
+    }
+ 
+export default Home;
