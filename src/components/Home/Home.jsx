1788d9c23baa16e667e6f6b6a5a6dc6c9e21abc0
Gregory Delgado <gregd92694@yahoo.com>
2021-12-08 09:46:04 -0600

    login page is complete. Just need to add axios functionality.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import "./Home.css";
 
 const Home=()=>{
         return (
+          <div>
             <section class ="home">
             <div class="card" >
                 <div class="card-body">
-                  <p class="card-text">Login</p>
+                <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Username/Email" variant="outlined" />
+      <div>
+      <TextField id="filled-basic" label="Password" variant="filled" />
+      <div className="button">
+      <Button variant="contained">Login</Button>
+      </div>
+      </div>
+    </Box>
+                </div>
+                <div>
+                <Button variant="text">Signup</Button>
+                </div>
+                <div>
+                <Button variant="text">Forgot Password</Button>
                 </div>
               </div>
         </section>
+        </div>
           );
     }
  
