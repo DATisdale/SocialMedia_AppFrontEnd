d151c21d8422550ed3ebdabf902dc0bf065eb551
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-12-08 10:13:56 -0600

    right sidebar

--- src/components/rightsidebar/rightsidebar.jsx
+++ src/components/rightsidebar/rightsidebar.jsx
@@ -1,6 +1,23 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import InputBase from '@mui/material/InputBase';
+// import Badge from '@mui/material/Badge';
+// import MenuItem from '@mui/material/MenuItem';
+// import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import SearchIcon from '@mui/icons-material/Search';
+// import AccountCircle from '@mui/icons-material/AccountCircle';
+// import MailIcon from '@mui/icons-material/Mail';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
+// import MoreIcon from '@mui/icons-material/MoreVert';
+
 
 export default function MultilineTextFields() {
   const [value, setValue] = React.useState('Controlled');
