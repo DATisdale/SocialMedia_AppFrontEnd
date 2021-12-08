685c088cf87bb455ab172ead7957d97b57deb30a
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-12-08 15:34:18 -0600

    new rightsidebar css and jsx code

--- src/components/rightsidebar/rightsidebar.jsx
+++ src/components/rightsidebar/rightsidebar.jsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import './rightsidebar.css'
 
-render () {
-  return  
-    <span className="rightSidebar">Right Sidebar</span>
-}
 
-render() {
-  let className = 'rightSidebar';
-  if (this.props.isActive) {
-    className += 'rightSidebar-active'
-  }
+const RightSidebar=()=>{
+  return(
+      <div className="RightSidebar">
+        <h1 className="Full Name"></h1>
+        <h2 className="Title"></h2>
+        <h3 className="favorite sport"></h3>
+        <h4 className="favorite team"></h4>
+        <button type="ADD"></button>
+      </div>  
+  )  
 }
 
 export default "rightsidebar";
