bca9e85e9daae0ff60c7f0e68683635a7ff03ee9
Gregory Delgado <gregd92694@yahoo.com>
2021-12-09 11:46:01 -0600

    Axios functionality works for creating a user

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
 import "./Signup.css"
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import axios from 'axios';
 
-const Signup =()=>{
+function Signup(props){
+
+  const [username,setUsername] = useState('');
+  const [email,setEmail] = useState('');
+  const[password, setPassword] = useState('');
+
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const postUser={
+            name: username,
+            email: email,
+            password: password,
+            isAdmin: false
+    };
+    let response = await axios.post(`http://localhost:5000/api/users/register`, postUser);
+    if(response.status == 200){
+      window.location = '/login'
+    }
+  }
+
+
     return(
-        <div className="signup">
-        <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-          <TextField
-            required
-            id="outlined-required"
-            label="Username"
-            defaultValue="Username"
-          />
-          <TextField
-            required
-            id="outlined-required"
-            label="Email"
-            defaultValue="Email"
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
-        </div>
-        <div className="button-container">
-        <Button variant="contained">Sign Up</Button>
-        </div>
-        
-      </Box>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <lable>Username</lable>
+        <input value={username} onChange={(event) => setUsername(event.target.value)} type='text' />
 
+        <lable>Password</lable>
+        <input value={password} onChange={(event) => setPassword(event.target.value)} type='text' />
+
+        <lable>email</lable>
+        <input value={email} onChange={(event) => setEmail(event.target.value)} type='text' />
+
+        <button type='submit'>Create User</button>
+      </form>
     )
 }
 
