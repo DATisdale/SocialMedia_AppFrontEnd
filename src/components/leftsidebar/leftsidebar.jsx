07a0bade1d5af9e080eac456f9b904e01f5479e0
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-12-09 09:39:40 -0600

    leftsidebar code

---
+++ src/components/leftsidebar/leftsidebar.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './leftsidebar.css'
+
+
+const LeftSidebar=()=>{
+  return(
+    <div className="LeftSidebar">
+          <div className="Fellow Fans">
+              {/* <h1 className="Fellow Fans">Fellow Fans</h1> */}
+              <h2 className="Title">Fellow Fans</h2>
+              <h3 className="favorite sport">Favorite Sport</h3>
+              <h3 className="favorite sports team">Favorite Sports Team</h3>
+          </div>
+          {/* <button type="ADD"></button> */}
+    </div>  
+  )  
+}
+
+export default LeftSidebar;
