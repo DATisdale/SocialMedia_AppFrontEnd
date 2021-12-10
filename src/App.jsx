9c2ab71024cccd6056dab89400e8da68287a2ebf
Gregory Delgado <gregd92694@yahoo.com>
2021-12-10 13:16:46 -0600

    login functionality success and ability to hold on generated token made possible.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import React, {Component} from "react";
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import SearchBar from "./components/Searchbar/Searchbar";
-import Home from "./components/Home/Home"
+import LoginForm from "./components/LoginForm/LoginForm"
 import Signup from "./components/Signup/Signup"
+import Home from './components/Home/Home'
+import jwtDecode from 'jwt-decode'
 // import PageLayout from "./components/PageLayout/PageLayout";
 
 class App extends Component {
-    state = {
-        username: null,
-        email: null,
-        password: null,
+    constructor(props){
+        super(props);
+        const jwt=localStorage.getItem('token');
+        try{
+          const decodedUser= jwtDecode(jwt);
+            this.state = {
+                user:decodedUser,
+            }
+        }catch{
+            this.state = {
+                user:null,
+            }
+        }
 
     }
 
     componentDidMount(){
         /// JWT maybe set other than null
+        const jwt=localStorage.getItem('token');
+        try{
+          const decodedUser= jwtDecode(jwt);
+          this.setState({user: decodedUser});
+        }catch{
+          
+        }
     }
 render() {
     return (
         <div className="App">
         <SearchBar/>
         <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact render={(props) => {
+                if(!this.state.user){
+                    return <Redirect to='/login' />
+                } else {
+                    return <Home {...props} />
+                }
+            }}/>
             <Route path="/signup" component={Signup}/>
+            <Route path="/login" component={LoginForm}/>
             {/* <Route path="/newsFeed" exact Component={NewsFeed}/>
             <Route path="/profile" render={(props) => {
                 if(!this.state.user){
