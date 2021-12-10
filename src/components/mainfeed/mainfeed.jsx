717aa42d0c28ddbd43145956c98ee1e1aeb248d8
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-10 16:15:12 -0500

    Started creating a display of mainfeed by setting the state.user to the decoded jwt.  Next step will be to access that state.users acceptedFriends array. (VPP, GD, DT, MS, JB).

--- src/components/mainfeed/mainfeed.jsx
+++ src/components/mainfeed/mainfeed.jsx
@@ -1,28 +1,56 @@
 import { BusAlertTwoTone } from '@mui/icons-material';
-import React from 'react';
-import './mainfeed.css'
+import React, { Component} from 'react';
+import './mainfeed.css';
+import jwtDecode from 'jwt-decode';
 
-const MainFeed=()=>{
-    return(
-        <div className="MainFeed">
-            <div className="Feed">
-                <h1 classname="user">User</h1>
-                <h4 className="status">Status</h4>
-                <button type="submit">Post</button>
+class MainFeed extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: {},
+        }
+    }
+    
+    JwtDecoder(){
+        const jwt = localStorage.getItem("token");
+        const decodedUser = jwtDecode(jwt);
+        this.state.user = decodedUser;
+        console.log(this.state.user);
+    }
+
+    // FriendsPosts(){
+    //     const findFriends = () => {
+    //         let usersFriends = this.state.user.acceptedFriends;
+    //         let friendsPosts = usersFriends.map(posts);
+    //         console.log(friendsPosts);
+    //     }
+    // }
+    
+render(){
+         return(
+            <div className="Post From Friends">
+                <div className="Feed">
+                    <h1 classname="user"></h1>
+                    <h4 className="status">Status</h4>
+               
+                </div>
+           
+                <div className="Likes and Dislikes">
+                    <hr />
+                    <h3 className="Like Posts">("")</h3>
+                    <button type="submit">Like</button> 
+                    <h3 className="Dislike Posts">("")</h3>
+                    <button type="submit">Dislike</button>
+                </div>
+           
+                <div className="Comments">
+                    <hr />
+                    <h3 className="Comments">Comments</h3>
+                    <h4 className="Write Comments">("")</h4>
+                    <button type="submit">Post Comment</button>  
+                </div>
             </div>
-            <div className="Likes and Dislikes">
-                <h3 className="Like Posts">("")</h3>
-                <button type="submit">Like</button> 
-                <h3 className="Dislike Posts">("")</h3>
-                <button type="submit">Dislike</button>
-            </div>
-            <div className="Comments">
-                <h3 className="Comments">Comments</h3>
-                <h4 className="Write Comments">("")</h4>
-                <button type="submit">Post Comment</button>  
-            </div>
-        </div>
-    )
+        )
+    }
 }
-
 export default MainFeed;
