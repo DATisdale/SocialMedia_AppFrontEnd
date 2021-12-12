31e9d0a035880a5837a94bf92a8b742cbc857474
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-12 14:33:46 -0500

    Created mainfeed class component that pulls in the jwt, decodes it, and then performs an axios call to retrieve user data to use on the mainfeed. (VPP, MS, DT, GD)

--- src/components/mainfeed/mainfeed.jsx
+++ src/components/mainfeed/mainfeed.jsx
@@ -2,43 +2,43 @@ import { BusAlertTwoTone } from '@mui/icons-material';
 import React, { Component} from 'react';
 import './mainfeed.css';
 import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+
 
 class MainFeed extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            user: {},
+        const jwt = localStorage.getItem('token');
+            const decodedUser = jwtDecode(jwt);
+            const currentUser = decodedUser._id;
+            this.state = {
+            userId: currentUser,
+            user: axios.get(`http://localhost:5000/api/users/current/${currentUser}`),
+            }
+    }
+    
+    
+    
+    FriendsPosts(){
+        const findFriends = () => {
+            let usersFriends = this.state.user.acceptedFriends;
+            console.log(usersFriends);
         }
     }
-    
-    JwtDecoder(){
-        const jwt = localStorage.getItem("token");
-        const decodedUser = jwtDecode(jwt);
-        this.state.user = decodedUser;
-        console.log(this.state.user);
-    }
 
-    // FriendsPosts(){
-    //     const findFriends = () => {
-    //         let usersFriends = this.state.user.acceptedFriends;
-    //         let friendsPosts = usersFriends.map(posts);
-    //         console.log(friendsPosts);
-    //     }
-    // }
-    
-render(){
-         return(
+    render() {
+        return(
             <div className="Post From Friends">
                 <div className="Feed">
                     <h1 classname="user"></h1>
                     <h4 className="status">Status</h4>
-               
                 </div>
            
                 <div className="Likes and Dislikes">
                     <hr />
                     <h3 className="Like Posts">("")</h3>
-                    <button type="submit">Like</button> 
+                    <button type="submit">Like{console.log(this.state.user)}</button>
+                    {/* <this.FriendsPosts />  */}
                     <h3 className="Dislike Posts">("")</h3>
                     <button type="submit">Dislike</button>
                 </div>
@@ -53,4 +53,5 @@ render(){
         )
     }
 }
+
 export default MainFeed;
